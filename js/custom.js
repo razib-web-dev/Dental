a7efc8f5dd95550f3588330306455b9bc0320c16
razib-dev <razibhossain344633@gmail.com>
2022-08-23 18:16:09 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.000000').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows:true,
+        prevArrow:'<i class="fa fa-chevron-left pre"</i>',
+        nextArrow:'<i class="fa fa-chevron-right nex"</i>',
+      });
+});
